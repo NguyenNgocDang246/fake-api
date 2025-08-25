856af27cde5abebfddc346b556b042b75a9427ee
NgocDang <ngocdang240604@gmail.com>
2025-08-25 10:03:47 +0700

    chore: rename variable (status -> statusCode)

--- src/core/api_response.ts
+++ src/core/api_response.ts
@@ -5,29 +5,29 @@ import { STATUS_CODE } from "@/core/constants";
 export default class ApiResponse {
   static success<T>({
     data,
-    status = STATUS_CODE.OK,
+    statusCode = STATUS_CODE.OK,
   }: {
     data: T;
-    status?: STATUS_CODE;
+    statusCode?: STATUS_CODE;
   }) {
     return NextResponse.json(
       { status: RESPONSE_STATUS.SUCCESS, data },
-      { status }
+      { status: statusCode }
     );
   }
 
   static error<T>({
     errors = null,
     message = ERROR_MESSAGES.SERVER_ERROR,
-    status = STATUS_CODE.SERVER_ERROR,
+    statusCode = STATUS_CODE.SERVER_ERROR,
   }: {
     errors?: T | null;
     message?: string;
-    status?: STATUS_CODE;
+    statusCode?: STATUS_CODE;
   } = {}) {
     return NextResponse.json(
       { status: RESPONSE_STATUS.ERROR, message, errors },
-      { status }
+      { status: statusCode }
     );
   }
 }
