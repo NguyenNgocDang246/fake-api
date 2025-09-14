346e608d92b750f60e35d7abb8b731174f5f14b2
NgocDang <ngocdang240604@gmail.com>
2025-09-15 00:48:01 +0700

    [FE] feat: update text font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Baloo_2 } from "next/font/google";
 import "@/app/styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+export const baloo2 = Baloo_2({
+  subsets: ["latin", "vietnamese"],
+  weight: ["500", "800"],
 });
 
 export const metadata: Metadata = {
@@ -24,11 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${baloo2.className} antialiased`}>{children}</body>
     </html>
   );
 }
