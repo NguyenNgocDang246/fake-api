15f52079b769ca0e84bad9aea404d0a91bcbcdbd
NgocDang <ngocdang240604@gmail.com>
2025-09-15 21:22:11 +0700

    [BE] feat: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import authMiddleware from "@/server/middlewares/auth.middleware";
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   // server
   if (url.pathname.startsWith("/api/auth/login")) return NextResponse.next();
   if (url.pathname.startsWith("/api/auth/register")) return NextResponse.next();
   if (url.pathname.startsWith("/api/auth/refresh-token")) return NextResponse.next();
-  if (url.pathname.startsWith("/api")) return authMiddleware();
-
-  // client
+  if (url.pathname.startsWith("/api")) return await authMiddleware();
 
   return NextResponse.next();
 }
