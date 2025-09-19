b3ac4ce71bbfbf2fa6657380702d2157f6497830
NgocDang <ngocdang240604@gmail.com>
2025-09-19 23:43:04 +0700

    [FE] feat: update home route at header

--- src/app/components/Wrapper/Header/GuestHeader.tsx
+++ src/app/components/Wrapper/Header/GuestHeader.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { NavigationButton } from "@/app/components/Button/NavigationButton";
+import { TextLink } from "@/app/components/Link/TextLink";
 import { PAGE_ROUTES } from "@/app/libs/routes";
 export function GuestHeader() {
   return (
     <div className="flex justify-between px-[6rem] py-4">
-      <div className="font-bold text-4xl">Fake API</div>
+      <TextLink href={PAGE_ROUTES.HOME} className="font-bold text-4xl">
+        Fake API
+      </TextLink>
       <div className="flex justify-between items-center gap-4">
         <NavigationButton href="./" label="Docs" />
         <NavigationButton href={PAGE_ROUTES.AUTH.LOGIN} label="Login" className="border-2" />
--- src/app/components/Wrapper/Header/UserHeader.tsx
+++ src/app/components/Wrapper/Header/UserHeader.tsx
@@ -1,17 +1,21 @@
 "use client";
 import { NavigationButton } from "@/app/components/Button/NavigationButton";
 import Image from "next/image";
-import { DropdownButton } from "../../Button/DropdownButton";
-import { LogoutButton } from "../../Button/LogoutButton";
-import { useAuth } from "../Auth/AuthWrapper";
+import { DropdownButton } from "@/app/components/Button/DropdownButton";
+import { LogoutButton } from "@/app/components/Button/LogoutButton";
+import { TextLink } from "@/app/components/Link/TextLink";
+import { useAuth } from "@/app/components/Wrapper/Auth/AuthWrapper";
+import { PAGE_ROUTES } from "@/app/libs/routes";
 
 export function UserHeader() {
   const { user } = useAuth();
   return (
     <div className="flex justify-between px-[6rem] py-4">
-      <div className="font-bold text-4xl">Fake API</div>
+      <TextLink href={PAGE_ROUTES.HOME} className="font-bold text-4xl">
+        Fake API
+      </TextLink>
       <div className="flex justify-between items-center gap-4">
-        <NavigationButton href="./" label="Docs" />
+        <NavigationButton href="" label="Docs" />
         <DropdownButton
           label={
             <Image
