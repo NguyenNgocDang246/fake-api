05663572784ff43ec2697845edabf5e490d7afef
NgocDang <ngocdang240604@gmail.com>
2025-09-26 18:50:32 +0700

    [FE] feat: implement api call for create project

--- src/app/project/components/CreateProjectForm/CreateProjectForm.tsx
+++ src/app/project/components/CreateProjectForm/CreateProjectForm.tsx
@@ -5,6 +5,10 @@ import { useForm } from "react-hook-form";
 import { ClientCreateProjectDTO, ClientCreateProjectSchema } from "@/models/project.model";
 import { FloatingInput } from "@/app/components/Input/FloatingInput";
 import { ErrorText } from "@/app/components/Text/ErrorText";
+import { API_ROUTES } from "@/app/libs/routes";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { ApiSuccessResponse, ApiErrorResponse } from "@/models/api_response.model";
+import api from "@/app/libs/helpers/api_call";
 
 export interface CreateProjectFormHandles {
   submit: () => Promise<boolean>;
@@ -15,13 +19,32 @@ export const CreateProjectForm = forwardRef<CreateProjectFormHandles>((props, re
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<ClientCreateProjectDTO>({
     resolver: zodResolver(ClientCreateProjectSchema),
   });
-  const onSubmit = (data: ClientCreateProjectDTO): Promise<boolean> => {
-    console.log("Form submitted:", data);
-    return Promise.resolve(true);
+  const queryClient = useQueryClient();
+  const createProjectMutation = useMutation<
+    ApiSuccessResponse,
+    ApiErrorResponse,
+    ClientCreateProjectDTO
+  >({
+    mutationFn: (data) => api.post(API_ROUTES.PROJECT.CREATE, data),
+    onSuccess() {
+      reset();
+      queryClient.invalidateQueries({ queryKey: ["projects"] });
+    },
+  });
+  const onSubmit = async (data: ClientCreateProjectDTO): Promise<boolean> => {
+    try {
+      await createProjectMutation.mutateAsync(data);
+      return true;
+    } catch (error) {
+      const data = (error as { data: ApiErrorResponse }).data;
+      void data;
+      return false;
+    }
   };
 
   useImperativeHandle(ref, () => ({
