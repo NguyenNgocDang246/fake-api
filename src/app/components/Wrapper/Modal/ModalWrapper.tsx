5fa0a53aebe874e4fef5947e3ba0a33cee715440
NgocDang <ngocdang240604@gmail.com>
2025-09-28 19:22:24 +0700

    [FE] feat: delete alert modal

--- src/app/components/Wrapper/Modal/ModalWrapper.tsx
+++ src/app/components/Wrapper/Modal/ModalWrapper.tsx
@@ -1,12 +1,10 @@
 "use client";
 import { createContext, useContext, useState } from "react";
 import { FormModalProps, FormModal } from "./FormModal";
-import { AlertModalProps, AlertModal } from "./AlertModal";
 import { ConfirmModalProps, ConfirmModal } from "./ConfirmModal";
 
 type ModalState =
   | { id: string; type: "form"; props: Omit<FormModalProps, "onClose"> }
-  | { id: string; type: "alert"; props: AlertModalProps }
   | { id: string; type: "confirm"; props: Omit<ConfirmModalProps, "onClose"> };
 
 interface ModalContextType {
@@ -32,23 +30,13 @@ export const ModalWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <ModalContext.Provider value={{ modals, openModal, closeModal }}>
       {children}
-      <div className="fixed bottom-4 right-4 z-50 h-screen flex flex-col-reverse gap-4">
-        {modals
-          .filter((m) => m.type === "alert")
-          .map((m) => {
-            return <AlertModal key={m.id} {...m.props} onClose={() => closeModal(m.id)} />;
-          })}
-      </div>
-
-      {modals
-        .filter((m) => m.type !== "alert")
-        .map((m) => {
-          if (m.type === "form")
-            return <FormModal key={m.id} {...m.props} onClose={() => closeModal(m.id)} />;
-          if (m.type === "confirm")
-            return <ConfirmModal key={m.id} {...m.props} onClose={() => closeModal(m.id)} />;
-          return null;
-        })}
+      {modals.map((m) => {
+        if (m.type === "form")
+          return <FormModal key={m.id} {...m.props} onClose={() => closeModal(m.id)} />;
+        if (m.type === "confirm")
+          return <ConfirmModal key={m.id} {...m.props} onClose={() => closeModal(m.id)} />;
+        return null;
+      })}
     </ModalContext.Provider>
   );
 };
