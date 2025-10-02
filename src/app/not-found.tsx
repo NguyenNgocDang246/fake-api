09259fd691eac7621d423ae9a9c2590ac7d74cb6
NgocDang <ngocdang240604@gmail.com>
2025-10-02 16:32:36 +0700

    [[FE] feat: implement not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,18 @@
-import Link from "next/link";
-
-export default function NotFound() {
+"use client";
+import { NavigationButton } from "@/app/components/Button/NavigationButton";
+import { PAGE_ROUTES } from "@/app/libs/routes";
+export default function Home() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="font-sans flex flex-col items-center min-h-screen px-6 py-12 bg-gray-50">
+      <h1 className="text-6xl font-medium text-black mb-4">404</h1>
+      <p className="text-lg text-gray-600 max-w-2xl text-center mb-8">
+        Sorry, we couldn&apos;t find the page you&apos;re looking for. The page might have been
+        moved or deleted.
+      </p>
+
+      <NavigationButton className="text-lg bg-blue-400" href={PAGE_ROUTES.HOME}>
+        Back to Home
+      </NavigationButton>
     </div>
   );
 }
