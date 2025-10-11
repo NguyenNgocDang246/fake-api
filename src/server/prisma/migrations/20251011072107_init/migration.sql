1d39738962063f4c5f3be3c96adb14967b4f6040
NgocDang <ngocdang240604@gmail.com>
2025-10-11 14:29:09 +0700

    [BE] feat: update schema

--- src/server/prisma/migrations/20251011072107_init/migration.sql
+++ src/server/prisma/migrations/20251011072107_init/migration.sql
@@ -1,5 +1,8 @@
+-- CreateEnum
+CREATE TYPE "HttpMethod" AS ENUM ('GET', 'POST', 'PUT', 'PATCH', 'DELETE');
+
 -- CreateTable
-CREATE TABLE "public"."endpoint_groups" (
+CREATE TABLE "endpoint_groups" (
     "id" BIGSERIAL NOT NULL,
     "project_id" BIGINT NOT NULL,
     "name" VARCHAR(100) NOT NULL,
@@ -8,20 +11,20 @@ CREATE TABLE "public"."endpoint_groups" (
 );
 
 -- CreateTable
-CREATE TABLE "public"."endpoints" (
+CREATE TABLE "endpoints" (
     "id" BIGSERIAL NOT NULL,
     "endpoint_groups_id" BIGINT NOT NULL,
-    "method" VARCHAR(10) NOT NULL,
+    "method" "HttpMethod" NOT NULL,
     "path" VARCHAR(255) NOT NULL,
-    "status_code" INTEGER DEFAULT 200,
+    "status_code" INTEGER NOT NULL DEFAULT 200,
     "response_body" JSONB,
-    "delay_ms" INTEGER DEFAULT 0,
+    "delay_ms" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "endpoints_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "public"."projects" (
+CREATE TABLE "projects" (
     "id" BIGSERIAL NOT NULL,
     "user_id" BIGINT NOT NULL,
     "name" VARCHAR(100) NOT NULL,
@@ -32,7 +35,7 @@ CREATE TABLE "public"."projects" (
 );
 
 -- CreateTable
-CREATE TABLE "public"."users" (
+CREATE TABLE "users" (
     "id" BIGSERIAL NOT NULL,
     "email" VARCHAR(100) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
@@ -40,18 +43,19 @@ CREATE TABLE "public"."users" (
     "token_version" BIGINT NOT NULL DEFAULT 0,
     "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
     "role" VARCHAR(50) NOT NULL DEFAULT 'USER',
+    "is_verified" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
-ALTER TABLE "public"."endpoint_groups" ADD CONSTRAINT "endpoint_groups_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "public"."projects"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE "endpoint_groups" ADD CONSTRAINT "endpoint_groups_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "projects"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "public"."endpoints" ADD CONSTRAINT "endpoints_endpoint_groups_id_fkey" FOREIGN KEY ("endpoint_groups_id") REFERENCES "public"."endpoint_groups"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE "endpoints" ADD CONSTRAINT "endpoints_endpoint_groups_id_fkey" FOREIGN KEY ("endpoint_groups_id") REFERENCES "endpoint_groups"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "public"."projects" ADD CONSTRAINT "projects_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE "projects" ADD CONSTRAINT "projects_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
