b195a5f7bc3dc750766d4d33a16972f250232433
NgocDang <ngocdang240604@gmail.com>
2025-10-12 18:03:38 +0700

    [FE] fix: fix example at home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <div className="mt-12 w-full max-w-xl bg-white shadow rounded-xl p-6">
         <h2 className="text-xl font-semibold mb-3">API Example</h2>
         <pre className="bg-gray-100 p-4 rounded-md text-sm overflow-x-auto">
-          {`GET ${DOMAIN}/api/v1/users\n\nResponse:\n{\n    "id": "1",\n    "name": "John Doe",\n    "email": "john@example.com"\n}`}
+          {`GET ${DOMAIN}/projectId/api/v1/users\n\nResponse:\n{\n    "id": "1",\n    "name": "John Doe",\n    "email": "john@example.com"\n}`}
         </pre>
       </div>
     </div>
